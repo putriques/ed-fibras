90f575540ecbfa18abc682213a370458e6f83928
putriques <kokushibosol5@gmail.com>
2025-05-21 17:26:58 -0300

    Ajuste na conexão com o bd

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,10 @@
 const knex = require('knex')
 const config = require('../../knexfile.js')
+require('dotenv').config()
 
-const db = knex(config.development)
+const environment = process.env.NODE_ENV || 'development'
+
+const db = knex(config[environment])
 
 db.raw('SELECT 1')
 .then(() => {
